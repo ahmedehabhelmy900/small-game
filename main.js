1a1dde75bc3a1d332c90d6e82dca8699e58c7712
ahmed <ahmedehab11@outlook.sa>
2023-12-02 23:01:00 +0400

    version 2

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 let bottun = document.querySelector("button");
 let span = document.querySelector("span");
+let press = 0;
+if (localStorage.getItem("presses")) {
+  press = localStorage.presses;
+  span.innerHTML = press;
+}
 bottun.onclick = () => {
-  window.alert("جدع");
-  span.innerHTML = parseInt(span.innerHTML) + 1;
+  if (!localStorage.getItem("presses")) {
+    localStorage.setItem("presses", 0);
+  }
+  press++;
+  window.alert(`دي الضغطة رقم ${press}`);
+  span.innerHTML = press;
+  localStorage.presses = press.toString();
 };
